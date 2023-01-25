ad4e496e58b5068a53c1a87e3e285a51f8c571e6
richguyniek <niek.esselbrugge@gmail.com>
2023-01-25 15:06:01 +0100

    step 1: array

---
+++ code/script.js
@@ -0,0 +1,14 @@
+//JS start template
+
+//array voor drumKit, bestaande uit de onderdelen die je met een loop moet ophalen in selected items
+const parts = [
+    "snare",
+    "high-hat",
+    "kick",
+    "crash",
+]
+
+
+
+
+
