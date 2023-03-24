249910d142de3b1c89d3d98513cfefd259ff3188
richguyniek <niek.esselbrugge@gmail.com>
2023-03-24 16:55:51 +0100

    array added

--- prototype 3/script.js
+++ prototype 3/script.js
@@ -1,17 +1,26 @@
-function allowDrop(allowdropevent) {
-    allowdropevent.target.style.color = "blue";
-    allowdropevent.preventDefault();
-  }
-  
-  function drag(dragevent) {
-    dragevent.dataTransfer.setData("text", dragevent.target.id);
-    dragevent.target.style.color = "green";
-  }
-  
-  function drop(dropevent) {
-    dropevent.preventDefault();
-    const data = dropevent.dataTransfer.getData("text");
-    dropevent.target.appendChild(document.getElementById(data));
-     document.getElementById("drag").style.color = "black";
-  }
 
+function allowDrop(allowdropevent) {
+  //allowdropevent.target.style.color = "blue";
+  allowdropevent.preventDefault();
+}
+  
+function drag(dragevent) {
+  dragevent.dataTransfer.setData("text", dragevent.target.id);
+  //dragevent.target.style.color = "green";
+}
+  
+function drop(dropevent) {
+  dropevent.preventDefault();
+  const data = dropevent.dataTransfer.getData("text");
+  dropevent.target.appendChild(document.getElementById(data));
+    // document.getElementById("drag").style.color = "red";
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var result = []; 
+  var matches = document.querySelectorAll("#drum_kit_layout_scripter div");
+  for (var i = 0, l = matches.length; i < l; i++){
+    result.push(matches[i].getAttribute("id")); 
+  }
+  console.log(result);
+});
